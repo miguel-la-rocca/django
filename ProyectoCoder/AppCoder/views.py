67fae1cfd0eb369932ba5a2fbd5a4b68d48e8561
miguel-la-rocca <108734320+miguel-la-rocca@users.noreply.github.com>
2022-09-27 23:24:13 -0300

    Add files via upload

---
+++ ProyectoCoder/AppCoder/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from AppCoder.models import Curso
+
+# Create your views here.
+def curso(request):
+    curso = Curso(nombre="Desarrollo web", camada= 19881)
+    curso.save()
+    documentoDeTexto= f"-----> Curso: {curso.nombre}   Camada: {curso.camada}"
+
+    return HttpResponse(documentoDeTexto)
+
+    
